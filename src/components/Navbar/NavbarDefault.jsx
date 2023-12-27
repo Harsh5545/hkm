e53b44f1f6d266fc01dfd7b99ca021d008257038
harshad <Harshadkajale333@gmail.com>
2023-12-27 20:40:11 +0530

    navbar editing

--- src/components/Navbar/NavbarDefault.jsx
+++ src/components/Navbar/NavbarDefault.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NavbarDefault() {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+    
+  };
+
   return (
-    <nav className="flex bg-black justify-between items-center px-5 py-5">
+    <nav className={`flex flex-col md:flex-row bg-black justify-between items-center px-10 py-5`}>
       <div>
         <h2 className="text-xl text-white font-semibold">Logo</h2>
       </div>
 
-      <ul className="flex items-center gap-2 text-[#bdbdc0] font-medium font-jakarta">
+      <div className="md:hidden">
+        {/* Mobile Menu Button */}
+        <button className="text-white" onClick={toggleMobileMenu}>
+          {isMobileMenuOpen ? '☰' : 'X'}
+        </button>
+      </div>
+
+      <ul className={`flex flex-col md:flex-row items-center gap-2 text-[#bdbdc0] font-medium font-jakarta`}>
         <li>
           <a href="" className="p-3 hover:text-white">
             Home
@@ -15,24 +29,24 @@ function NavbarDefault() {
         </li>
         <li>
           <a href="" className="p-3 hover:text-white">
-            About Us
+            Features
           </a>
         </li>
         <li>
           <a href="" className="p-3 hover:text-white">
-            Course
+            Pricing
           </a>
         </li>
         <li>
           <a href="" className="p-3 hover:text-white">
-            Student Project
+            Contact
           </a>
         </li>
       </ul>
 
-      <div>
+      <div className="hidden md:block">
         <button className="font-jakarta text-sm px-4 py-2 rounded-full text-white font-medium bg-white bg-opacity-[.08] border border-white border-opacity-[.08] hover:border-opacity-25">
-          Enroll
+          Log in
         </button>
       </div>
     </nav>
--- src/components/Navbar/NavbarFixed.jsx
+++ src/components/Navbar/NavbarFixed.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion }  from "framer-motion";
+import { motion } from "framer-motion";
 
 const navVariants = {
   initial: {
@@ -12,7 +12,7 @@ const navVariants = {
     x: "-50%",
     opacity: 1,
     transition: {
-      duration: 0.2,
+      duration: 0.5, // Increase the duration to slow down the motion
     },
   },
   exit: {
@@ -28,35 +28,41 @@ function NavbarFixed() {
       animate="animate"
       exit="exit"
       variants={navVariants}
-      className="fixed z-[999] top-2 left-1/2 -translate-x-1/2 rounded-full p-1 bg-black bg-opacity-[.99] backdrop-blur-lg border border-white border-opacity-[.08]"
+      className="fixed z-[999] top-2 left-1/2 -translate-x-1/2 rounded-full p-2 bg-black bg-opacity-[.8] backdrop-blur-lg border border-white border-opacity-[.08] flex"
+      style={{ width: "80%" }} // Increase the width to your desired value
     >
-      <ul className="flex items-center gap-2 text-sm font-medium font-jakarta text-white">
-        <li>
-          <a href="#" className="p-4 hover:opacity-50">
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="#" className="p-4 hover:opacity-50">
-            AboutUs
-          </a>
-        </li>
-        <li>
-          <a href="#" className="p-4 hover:opacity-50">
-            Course
-          </a>
-        </li>
-        <li>
-          <a href="#" className="p-4 hover:opacity-50">
-            Student Project
-          </a>
-        </li>
-        <li>
-          <button className="font-jakarta text-sm px-4 py-2 rounded-3xl text-white font-medium bg-[#171719] border border-white border-opacity-[.08] hover:border-opacity-25">
-            Enroll
-          </button>
-        </li>
-      </ul>
+      <div className="flex items-center justify-between w-full"> {/* Added justify-between and w-full */}
+        <div>
+          <h2 className="text-xl text-white font-semibold">Logo</h2>
+        </div>
+        <ul className="flex items-center gap-2 text-sm font-medium font-jakarta text-white">
+          <li>
+            <a href="#" className="p-4 hover:opacity-50">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className="p-4 hover:opacity-50">
+              AboutUs
+            </a>
+          </li>
+          <li>
+            <a href="#" className="p-4 hover:opacity-50">
+              Course
+            </a>
+          </li>
+          <li>
+            <a href="#" className="p-4 hover:opacity-50">
+              Student Project
+            </a>
+          </li>
+          <li>
+            <button className="font-jakarta text-sm px-4 py-2 rounded-3xl text-white font-medium bg-[#171719] border border-white border-opacity-[.08] hover:border-opacity-25">
+              Login
+            </button>
+          </li>
+        </ul>
+      </div>
     </motion.div>
   );
 }
