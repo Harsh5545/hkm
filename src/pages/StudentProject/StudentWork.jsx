c32cf9d43780913b972f3966f084e21ebe2f262e
harshad <Harshadkajale333@gmail.com>
2024-01-11 18:25:30 +0530

    chn

--- src/pages/StudentProject/StudentWork.jsx
+++ src/pages/StudentProject/StudentWork.jsx
@@ -184,8 +184,8 @@ function StudentWork() {
                 strokeWidth="2"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M15 19l-7-7 7-7"
                 />
               </svg>
@@ -222,11 +222,11 @@ function StudentWork() {
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
+                strokeWidth="2"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M9 5l7 7-7 7"
                 />
               </svg>
