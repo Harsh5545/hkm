8794e8e35faf6a5a8e29ece7bc13b7ea2df3d74c
harshad <Harshadkajale333@gmail.com>
2024-01-25 11:53:06 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,8 @@ import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Loader from "./components/Loader/Loader";
-
 import Footer from "./components/Footer/Footer";
-
-// Lazy-loaded components
-const GameDesign = lazy(() => import("./pages/Course/CoursePages/GameDesign"));
-const Jewellery = lazy(() => import("./pages/Course/CoursePages/Jewellery"));
-const Architectural = lazy(() =>
-  import("./pages/Course/CoursePages/Architectural")
-);
-const VideoEditing = lazy(() =>
-  import("./pages/Course/CoursePages/VideoEditing")
-);
-const Animation3D = lazy(() =>
-  import("./pages/Course/CoursePages/Animation3D")
-);
-const VisualEffect = lazy(() =>
-  import("./pages/Course/CoursePages/VisualEffect")
-);
-const Animation2D = lazy(() =>
-  import("./pages/Course/CoursePages/Animation2D")
-);
-const GraphicsDesign = lazy(() =>
-  import("./pages/Course/CoursePages/GraphicsDesign")
-);
-const PGA = lazy(() => import("./pages/Course/CoursePages/PGA"));
-const PGD = lazy(() => import("./pages/Course/CoursePages/PGD"));
+import dynamicRoutes from "./Route";
 const Home = lazy(() => import("./pages/Home/Home"));
 const Aboutus = lazy(() => import("./pages/About/Aboutus"));
 const Course = lazy(() => import("./pages/Course/Course"));
@@ -35,9 +11,18 @@ const StudentProject = lazy(() =>
   import("./pages/StudentProject/StudentProject")
 );
 const Contact = lazy(() => import("./pages/Contact/Contact"));
+const generatedRoutes = dynamicRoutes.map((route) => {
+  return {
+    path: `/course/${route.slug}`,
+    element: (
+      <Suspense fallback={<Loader />}>
+        {React.createElement(route.component)}
+      </Suspense>
+    ),
+  };
+});
 
-// Route configuration array
-const routes = [
+const Staticroutes = [
   {
     path: "/",
     element: (
@@ -83,94 +68,15 @@ const routes = [
       </Suspense>
     ),
   },
-  {
-    path: "/course/game-design",
-    element: (
-      <Suspense fallback={<Loader />}>
-        <GameDesign />
-      </Suspense>
-    ),
-  },
-  {
-    path: "/course/3d-jewelry-design",
-    element: (
-      <Suspense fallback={<Loader />}>
-        <Jewellery />
-      </Suspense>
-    ),
-  },
-  {
-    path: "/course/3d-architectural",
-    element: (
-      <Suspense fallback={<Loader />}>
-        <Architectural />
-      </Suspense>
-    ),
-  },
-  {
-    path: "/course/video-editing",
-    element: (
-      <Suspense fallback={<Loader />}>
-        <VideoEditing />
-      </Suspense>
-    ),
-  },
-  {
-    path: "/course/3d-animation",
-    element: (
-      <Suspense fallback={<Loader />}>
-        <Animation3D />
-      </Suspense>
-    ),
-  },
-  {
-    path: "/course/visual-effect",
-    element: (
-      <Suspense fallback={<Loader />}>
-        <VisualEffect />
-      </Suspense>
-    ),
-  },
-  {
-    path: "/course/2d-animation",
-    element: (
-      <Suspense fallback={<Loader />}>
-        <Animation2D />
-      </Suspense>
-    ),
-  },
-  {
-    path: "/course/graphics-design",
-    element: (
-      <Suspense fallback={<Loader />}>
-        <GraphicsDesign />
-      </Suspense>
-    ),
-  },
-  {
-    path: "/course/graphics-animation-program",
-    element: (
-      <Suspense fallback={<Loader />}>
-        <PGA />
-      </Suspense>
-    ),
-  },
-  {
-    path: "/course/game-design-program",
-    element: (
-      <Suspense fallback={<Loader />}>
-        <PGD />
-      </Suspense>
-    ),
-  },
 ];
+const allRoutes = [...Staticroutes, ...generatedRoutes];
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        {routes.map((route) => (
+        {allRoutes.map((route) => (
           <Route key={route.path} path={route.path} element={route.element} />
         ))}
       </Routes>
